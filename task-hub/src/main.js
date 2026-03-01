884349fcea3a5081a905411655deea27941d4647
Huauaua <1775778681@qq.com>
2026-03-01 23:18:22 +0800

    day7: 任务清单

---
+++ task-hub/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import './style.css'
+import router from "./router";
+
+const app = createApp(App)
+app.use(createPinia()) // 插件注册
+app.use(router)
+app.mount('#app')
