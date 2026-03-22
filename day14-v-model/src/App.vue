819e736a5c3fc30ed2d674feede7102de61c6543
Huauaua <1775778681@qq.com>
2026-03-22 23:47:15 +0800

    day14: v-model组件封装

---
+++ day14-v-model/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <Form
+    :cityId="cityId"
+    @change="handleChange"
+  ></Form>
+</template>
+
+<script>
+import Form from "./components/Form.vue";
+export default {
+  components: {
+    Form
+  },
+  data() {
+    return {
+      cityId: 1
+    }
+  },
+  methods: {
+    handleChange(e) {
+      this.cityId = Number(e.target.value)
+    }
+  }
+}
+
+</script>
---
+++ day14-v-model/src/components/Form.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "form",
+  props:{
+    cityId: Number
+  },
+  methods:{
+    handleChange(e: any){
+      this.$emit('change', Number(e.target.value))
+    }
+  }
+})
+</script>
+
+<template>
+<div>
+  <select :value="cityId" @change="handleChange">
+    <option value="1">北京</option>
+    <option value="2">上海</option>
+    <option value="3">天津</option>
+    <option value="4">河北</option>
+    <option value="5">山西</option>
+    <option value="6">内蒙古</option>
+  </select>
+</div>
+</template>
+
+<style scoped>
+
+</style>
