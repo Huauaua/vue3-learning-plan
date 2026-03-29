9bc2a8527328994e3a06a942444438dfb8c3ade3
Huauaua <1775778681@qq.com>
2026-03-29 23:54:21 +0800

    day19: slot自定义插槽

---
+++ day17-slot/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import MyDialog from "./components/MyDialog.vue";
+</script>
+
+<template>
+  <div>
+    <MyDialog></MyDialog>
+  </div>
+</template>
---
+++ day17-slot/src/components/MyDialog.vue
@@ -0,0 +1,77 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "MyDialog"
+})
+</script>
+
+<template>
+  <div class="dialog">
+    <div class="dialog-header">
+      <h3>友情提示</h3>
+      <span class="close"> X </span>
+    </div>
+
+    <div class="dialog-content">
+      你确定要删除吗？
+    </div>
+    <div class="dialog-footer">
+      <button class="cancel">取消</button>
+      <button class="confirm">确定</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .dialog {
+    width: 300px;
+    height: 200px;
+    background-color: #fff;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  .dialog-header {
+    height: 50px;
+    background-color: #f4f4f4;
+    border-bottom: 1px solid #ccc;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 10px;
+  }
+  .dialog-content {
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .dialog-footer {
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 0 10px;
+  }
+  .dialog-footer button {
+    margin-left: 10px;
+  }
+  .dialog-footer .cancel {
+    background-color: #ccc;
+    border: 1px solid #ccc;
+  }
+  .dialog-footer .confirm {
+    background-color: #42b983;
+    border: 1px solid #42b983;
+  }
+  .dialog-footer button:hover {
+    opacity: 0.8;
+  }
+  .dialog-footer button:active {
+    opacity: 1;
+  }
+</style>
