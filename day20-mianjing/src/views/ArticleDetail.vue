05cf663f2f092b22f1f68323db9dea4bed56216c
Huauaua <1775778681@qq.com>
2026-04-16 00:36:18 +0800

    day26: 面经基础版

---
+++ day20-mianjing/src/views/ArticleDetail.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "ArticleDetail"
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
